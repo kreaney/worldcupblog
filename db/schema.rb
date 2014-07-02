f5bf6c239072ea184594ea2d48d044133ee22c0c
Kevin Reaney <kevin.reaney@ngahr.com>
2014-07-02 22:44:29 +0100

    Commitment of final draft without date filter

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623170519) do
+ActiveRecord::Schema.define(version: 20140702202839) do
 
   create_table "articles", force: true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "title"
+    t.text      "body"
+    t.datetime  "created_at"
+    t.datetime  "updated_at"
+    t.timestamp "publishdate"
   end
 
   create_table "comments", force: true do |t|
